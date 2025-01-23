00bff3b084a2f294312da88034d2c48ac113ed0f
Мария Шабалина <masha290208@gmail.com>
2025-01-23 12:18:29 +0500

    chto-to sdelal vrode

--- main.cpp
+++ main.cpp
@@ -1,20 +1,11 @@
 #include <iostream>
 #include <Windows.h>
 
-int NOD(int num1, int num2) {
-	if (num1 < num2) {
-		std::swap(num1, num2);
-	}
-	while (num1 % num2 != 0) {
-		num1 = num1 % num2;
-		std::swap(num1, num2);
-	}
-	return num2;
-}
 
-int NOK(int num1, int num2) {
+
+/*int NOK(int num1, int num2) {
 	return num1 * num2 / NOD(num1, num2);
-}
+}*/
 
 
 class Integer {
@@ -23,63 +14,45 @@ public:
 	//способы создания объектов
 
 	Integer() :Integer(false, 0u) {}
-	Integer(bool sign, unsigned untils) :sign_(sign), untils_(untils) {}
-	Integer(bool sign, unsigned untils, bool sign2, unsigned untils2) :sign_(sign), untils_(untils), sign2_(sign), untils2_(untils) {}
+	Integer(bool sign, unsigned units) :sign_(sign), units_(units) {}
 	Integer(int number) {
 		if (number < 0)
 		{
 			sign_ = true;
-			untils_ = ::abs(number);
+			units_ = ::abs(number);
 		}
 		else {
 			sign_ = false;
-			untils_ = static_cast<unsigned>(number);
-		}
-	}
-	Integer(int number1, int number2) {
-		if (number1 < 0)
-		{
-			sign_ = true;
-			untils_ = ::abs(number1);
-		}
-		else {
-			sign_ = false;
-			untils_ = static_cast<unsigned>(number1);
-		}
-		
-		if (number2 < 0)
-		{
-			sign2_ = true;
-			untils2_ = ::abs(number2);
-		}
-		else {
-			sign2_ = false;
-			untils2_ = static_cast<unsigned>(number2);
+			units_ = static_cast<unsigned>(number);
 		}
 	}
 
 	//способы обработки объектов
 
-	void SetSign(bool sign) {
-		this->sign_ = sign;
+	void SetSign(bool newSign) {
+		this->sign_ = newSign;
 	}
 
-	void SetUntils(unsigned untils) {
-		this->untils_ = untils;
+	void SetUnits(unsigned newunits) {
+		this->units_ = newunits;
 	}
 
 	bool GetSign()const {
 		return sign_;
 	}
 
-	unsigned GetUntils()const {
-		return untils_;
+	unsigned GetUnits()const {
+		return units_;
+	}
+
+	void Print()const {
+		std::cout << (sign_ ? '-' : ' ') << units_ << '\n';
 	}
 
 	//способы проверки состояния
 
 	bool isOdd()const {
-		return bool(untils_ % 2);
+		return !bool(units_ % 2);
 	}
 
 	bool isPositive()const {
@@ -87,41 +60,78 @@ public:
 	}
 
 	bool isPrime()const {
-		for (int i = 2; i <= sqrt(untils_); i++) {
-			if (untils_ % i == 0) {
+		for (int i = 2; i <= sqrt(units_); i++) {
+			if (units_ % i == 0) {
 				return 0;
 			}
 		}
 		return 1;
 	}
 
-	bool isСoprime()const {
-		if (NOD(untils_, untils2_) == 1)
+	/*bool isСoprime()const {
+		if (NOD(units_) == 1)
 		{
 			return 1;
 		}
 		return 0;
-	}
+	}*/
+
+	/*int NOD(Integer a, Integer b) {
+		if (a < b) {
+			std::swap(a, b);
+		}
+		while (a % b != 0) {
+			a = a % b;
+			std::swap(a, b);
+		}
+		return b;
+	}*/
 
 	//перегрузки операторов
 
-	Integer operator+(Integer other)const {
+	Integer operator+(const Integer& other)const {
+		Integer result;
+		if (sign_ == other.sign_) {
+			result.sign_ = sign_;
+			result.units_ = units_ + other.units_;
+		}
+		else
+		{
+			if (units_ > other.units_)
+			{
+				result.sign_ = sign_;
+				result.units_ = units_ - other.units_;
+			}
+			else if (units_ < other.units_) {
+				result.sign_ = other.sign_;
+				result.units_ = other.units_ - units_;
+			}
+			else {
+				result.sign_ = false;
+				result.units_ = true;
+			}
+		}
+		return result;
+	}
+
+	friend Integer operator-(const Integer& a, const Integer& b) {
+		Integer tmp = b;
+		tmp.sign_ = !tmp.sign_;
+		return a + tmp;
+	}
+
+	friend Integer operator*(Integer a, Integer b) {
 		return Integer();
 	}
 
-	friend Integer operator*(Integer a, Integer b);
+	
+
 
 private:
 	bool sign_;
-	unsigned untils_;
-
-	bool sign2_;
-	unsigned untils2_;
-
+	unsigned units_;
 };
-Integer operator*(Integer a, Integer b) {
-	return Integer();
-}
+
 
 
 
@@ -129,17 +139,18 @@ int main() {
 
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-		
-	int num1 = 37;
-	int num2 = 42;
 
-	std::cout << num1 << " не четное(1) или четное(0): " << Integer(num1).isOdd() << '\n';
-	std::cout << num1 << " положительное(1) или отрицательное(0): " << Integer(num1).isPositive() << '\n';
-	std::cout << num1 << " простое(1) или сложное(0): " << Integer(num1).isPrime() << '\n';
-	std::cout << num1 << " и " << num2 << " взаимнопростые(1) или взаимно сложные(0): " << Integer(num1, num2).isСoprime() << '\n';
+	Integer num1{ !37 };
+	Integer num2{ -42 };
 	
-	std::cout << "Наибольший общий делитель " << num1 << " и " << num2 << " : " << NOD(Integer(num1).GetUntils(), Integer(num2).GetUntils()) << '\n';
-	//std::cout << "Наименьшее общее кратное " << num1 << " и " << num2 << " : " << NOK(Integer(num1).GetUntils(), Integer(num2).GetUntils()) << '\n';
+	num1.Print();
+	std::cout << (num1.isOdd() ? "Четное" : "Не четное") << '\n';
+	std::cout << (num1.isPositive() ? "Положительное" : "Отрицательное") << '\n';
+	std::cout << (num1.isPrime() ? "Простое" : "Сложное") << '\n';
+	//std::cout << num1 << " и " << num2 << " взаимнопростые(1) или взаимносложные(0): " << num1, num2.isСoprime() << '\n';
+	
+	//std::cout << "Наибольший общий делитель " << num1 << " и " << num2 << " : " << NOD(num1, num2) << '\n';
+	//std::cout << "Наименьшее общее кратное " << num1 << " и " << num2 << " : " << NOK(Integer(num1).Getunits(), Integer(num2).Getunits()) << '\n';
 	
 	return 0;
 }
